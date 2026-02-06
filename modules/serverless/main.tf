3ac934e3342eeccee1af075ae0d143671f8ff575
Your Name <your-email@example.com>
2026-02-06 17:05:39 +0200

    SECURITY: Changed API Gateway authorization from NONE to AWS_IAM

--- modules/serverless/main.tf
+++ modules/serverless/main.tf
@@ -137,7 +137,7 @@ resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.main.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
-  authorization = "NONE"
+  authorization = "AWS_IAM"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
